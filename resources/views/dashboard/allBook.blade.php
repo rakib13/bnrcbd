b8225168a7f97a9bf67ffad0725bbb17396014a9
rakib13 <rakib.omar13@gmail.com>
2025-08-03 17:23:33 +0600

    ICon Update

--- resources/views/dashboard/allBook.blade.php
+++ resources/views/dashboard/allBook.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card shadow border-0">
                 <div class="card-body p-3">
-                    <h3 class="text-center text-primary mb-4"><span class=" border-bottom">List of Book</span></h3>
+                    <h3 class="text-center text-primary mb-4 border-bottom">List of Book</h3>
 
                     <table class="table table-striped p-0 m-0 w-0">
                         <thead class="text-center">
@@ -17,7 +17,7 @@
                             <th>Is Shown</th>
                             <th>Actions</th>
                         </thead>
-                        <tbody>
+                        <tbody class="text-center">
                             @foreach ($publish as $count => $row)
                                 <tr>
                                     <th>{{ ++$count }}.</th>
@@ -25,12 +25,13 @@
                                     <th>{{ $row->book_author }}</th>
                                     <th>{{ $row->publish_date }}</th>
                                     <th>{{ $row->category_of_publication }}</th>
-                                    <th><i class="fas {{ $row->is_visible ? 'fa-check-circle' : 'fa-do-not-enter' }}"></i></th>
+                                    <th><i id="" class="fas {{ $row->is_visible ? 'fa-eye' : 'fa-eye-slash' }}"></i></th>
                                     <th>
-                                        <a class="btn btn-success" href="{{ url('/show-book/' . $row->id) }}"><i
-                                                class="fa fa-eye" aria-hidden="true"></i></a>
-                                        <a class="btn btn-warning" href="{{ url('/edit-book/' . $row->id) }}"><i
-                                                class="fas fa-edit"></i></a>
+                                        <a class="btn btn-success" href="{{ url('/show-book/' . $row->id) }}">
+                                            <i class="fas fa-binoculars"></i>
+                                        </a>
+                                        <a class="btn btn-warning" href="{{ url('/edit-book/' . $row->id) }}">
+                                            <i class="fas fa-edit"></i></a>
                                         <a class="btn btn-danger"><i class="fas fa-trash"></i></a>
                                     </th>
                                 </tr>
